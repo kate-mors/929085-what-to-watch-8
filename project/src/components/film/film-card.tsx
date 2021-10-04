c7aa6cd079ad34c6742c28dbcb5647de05816f87
kate-mors <ekaterina.morsino@gmail.com>
2021-10-04 11:17:33 +0200

    Render list of FilmCard into Main

--- project/src/components/film/film-card.tsx
+++ project/src/components/film/film-card.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 
-function FilmCard(): JSX.Element {
+type FilmProps = {
+  titleItem: string;
+}
+
+function FilmCard({titleItem}:FilmProps): JSX.Element {
   return (
     <React.Fragment>
       <article className="small-film-card catalog__films-card">
         <div className="small-film-card__image">
-          <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175" />
+          <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt={titleItem} width="280" height="175" />
         </div>
         <h3 className="small-film-card__title">
-          <a className="small-film-card__link" href="film-page.html">Fantastic Beasts: The Crimes of Grindelwald</a>
+          <a className="small-film-card__link" href="film-page.html">{titleItem}</a>
         </h3>
       </article>
     </React.Fragment>
   )
 }
+
+export default FilmCard;
