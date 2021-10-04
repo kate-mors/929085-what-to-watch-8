a50f8372533343206171a583eb9db647a73c756b
kate-mors <ekaterina.morsino@gmail.com>
2021-10-04 11:45:40 +0200

    Render App into #root

--- project/src/components/film/film-card.tsx
+++ project/src/components/film/film-card.tsx
@@ -1,22 +1,18 @@
-import React from "react";
-
 type FilmProps = {
   titleItem: string;
 }
 
 function FilmCard({titleItem}:FilmProps): JSX.Element {
   return (
-    <React.Fragment>
-      <article className="small-film-card catalog__films-card">
-        <div className="small-film-card__image">
-          <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt={titleItem} width="280" height="175" />
-        </div>
-        <h3 className="small-film-card__title">
-          <a className="small-film-card__link" href="film-page.html">{titleItem}</a>
-        </h3>
-      </article>
-    </React.Fragment>
-  )
+    <article className="small-film-card catalog__films-card">
+      <div className="small-film-card__image">
+        <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt={titleItem} width="280" height="175" />
+      </div>
+      <h3 className="small-film-card__title">
+        <a className="small-film-card__link" href="film-page.html">{titleItem}</a>
+      </h3>
+    </article>
+  );
 }
 
 export default FilmCard;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const titlesList = ['Fantastic Beasts: The Crimes of Grindelwald', 'Bohemian Rhapsody', 'Macbeth', 'Aviator', 'We need to talk about Kevin', 'What We Do in the Shadows', 'Revenant', 'Johnny English', 'Shutter Island', 'Pulp Fiction', 'No Country for Old Men', 'Snatch', 'Moonrise Kingdom', 'Seven Years in Tibet', 'Midnight Special', 'War of the Worlds', 'Dardjeeling Limited', 'Orlando', 'Mindhunter', 'Midnight Special'];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App titlesList = {titlesList}/>
   </React.StrictMode>,
   document.getElementById('root'));
