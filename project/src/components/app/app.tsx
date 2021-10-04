9817da6ffc5635999ec0186f748ca05d246807c2
kate-mors <ekaterina.morsino@gmail.com>
2021-10-04 11:20:51 +0200

    Updating App component

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../main/main';
+
+type AppProps = {
+  titlesList: string[];
+}
+
+function App({titlesList}:AppProps): JSX.Element {
+  return <Main titlesList={titlesList}/>;
 }
 
 export default App;
