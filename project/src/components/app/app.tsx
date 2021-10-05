c3f53bb15eca5ca13eafcd280d57f69c94b3d38b
kate-mors <ekaterina.morsino@gmail.com>
2021-10-05 15:17:51 +0200

    Import react-router-dom & Route components

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Main from '../main/main';
+import AddReview from '../add-review/add-review';
+import Film from '../film/film';
+import MyList from '../my-list/my-list';
+import PageNotFound from '../page-not-found/page-not-found';
+import Player from '../player/player';
+import SignIn from '../sign-in/sign-in';
 
 type AppProps = {
   titlesList: string[];
@@ -8,7 +16,38 @@ type AppProps = {
 }
 
 function App({titlesList, filmTitle, filmGenre, filmYear}:AppProps): JSX.Element {
-  return <Main titlesList={titlesList} filmTitle={filmTitle} filmGenre={filmGenre} filmYear={filmYear} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Main
+            titlesList={titlesList}
+            filmTitle={filmTitle}
+            filmGenre={filmGenre}
+            filmYear={filmYear}
+          />
+        </Route>
+        <Route path="/login" exact>
+          <SignIn />
+        </Route>
+        <Route path="/mylist" exact>
+          <MyList />
+        </Route>
+        <Route path="/films/:id?" exact>
+          <Film />
+        </Route>
+        <Route path="/films/:id/review" exact>
+          <AddReview />
+        </Route>
+        <Route path="/player/:id" exact>
+          <Player />
+        </Route>
+        <Route>
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
