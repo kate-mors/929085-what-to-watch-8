c2202c48972d68ec6d3871d4dfcd6d6489b0fe7c
kate-mors <ekaterina.morsino@gmail.com>
2021-10-10 17:36:51 +0200

    fixup! Create mocks

--- project/src/mocks/reviews.ts
+++ project/src/mocks/reviews.ts
@@ -1,37 +1,52 @@
 export const reviews = [
   {
     'id': 1,
-    'user': 'Kate Muir',
+    'user': {
+      'id': 4,
+      'name': 'Kate Muir',
+    },
     'rating': 8.9,
     'comment': 'Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director\'s funniest and most exquisitely designed movies in years.',
-    'date': 'December 24, 2016',
+    'date': '2019-05-08T14:13:56.569Z',
   },
   {
     'id': 2,
-    'user': 'Bill Goodykoontz',
+    'user': {
+      'id': 2,
+      'name': 'Bill Goodykoontz',
+    },
     'rating': 8,
     'comment': 'Anderson\'s films are too precious for some, but for those of us willing to lose ourselves in them, they\'re a delight. "The Grand Budapest Hotel" is no different, except that he has added a hint of gravitas to the mix, improving the recipe.',
-    'date': 'November 18, 2015',
+    'date': '2015-11-18T14:13:56.569Z',
   },
   {
     'id': 3,
-    'user': 'Amanda Greever',
+    'user': {
+      'id': 6,
+      'name': 'Amanda Greever',
+    },
     'rating': 8,
     'comment': 'I didn\'t find it amusing, and while I can appreciate the creativity, it\'s an hour and 40 minutes I wish I could take back.',
-    'date': 'November 18, 2015',
+    'date': '2015-11-18T14:13:56.569Z',
   },
   {
     'id': 5,
-    'user': 'Matthew Lickona',
+    'user': {
+      'id': 5,
+      'name': 'Matthew Lickona',
+    },
     'rating': 7.2,
     'comment': 'The mannered, madcap proceedings are often delightful, occasionally silly, and here and there, gruesome and/or heartbreaking.',
-    'date': 'December 20, 2016',
+    'date': '2016-12-20T14:13:56.569Z',
   },
   {
     'id': 6,
-    'user': 'Paula Fleri-Soler',
+    'user': {
+      'id': 3,
+      'name': 'Paula Fleri-Soler',
+    },
     'rating': 7.6,
     'comment': 'It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.',
-    'date': 'December 20, 2016',
+    'date': '2016-12-20T14:13:56.569Z',
   },
 ];
