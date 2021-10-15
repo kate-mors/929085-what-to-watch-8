7cb072c4965b634468e6905a3e7c6bdd7d8e3e85
kate-mors <ekaterina.morsino@gmail.com>
2021-10-15 22:11:57 +0200

    fixup! Create review-form, add review props

--- project/src/components/review-form/review-form.tsx
+++ project/src/components/review-form/review-form.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { useState, FormEvent } from 'react';
 
 function ReviewForm(): JSX.Element {
+  const ratingStars = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
   const [checked, setChecked] = useState(false);
   const [text, setText] = useState('');
 
-  const checkboxToggle = (state:boolean) => !state;
+  const handleChange = () => setChecked((state) => !state);
 
   const handleSubmit = (evt:FormEvent<HTMLFormElement >) => {
     evt.preventDefault();
@@ -15,135 +16,23 @@ function ReviewForm(): JSX.Element {
     <form action="#" className="add-review__form" onSubmit={handleSubmit}>
       <div className="rating">
         <div className="rating__stars">
-          <input
-            className="rating__input"
-            id="star-10"
-            type="radio"
-            name="rating"
-            value="10"
-            checked={checked}
-            onChange={() => setChecked(checkboxToggle)}
-          />
-          <label className="rating__label" htmlFor="star-10">
-            Rating 10
-          </label>
+          {ratingStars.map((star) => (
+            <>
+              <input
+                className="rating__input"
+                id={`star-${star}`}
+                type="radio"
+                name="rating"
+                value={star}
+                checked={checked}
+                onChange={handleChange}
+              />
+              <label className="rating__label" htmlFor={`star-${star}`}>
+                Rating {star}
+              </label>
+            </>
+          ))}
 
-          <input
-            className="rating__input"
-            id="star-9"
-            type="radio"
-            name="rating"
-            value="9"
-            checked={checked}
-            onChange={() => setChecked(checkboxToggle)}
-          />
-          <label className="rating__label" htmlFor="star-9">
-            Rating 9
-          </label>
-
-          <input
-            className="rating__input"
-            id="star-8"
-            type="radio"
-            name="rating"
-            value="8"
-            checked={!checked}
-            onChange={() => setChecked(checkboxToggle)}
-          />
-          <label className="rating__label" htmlFor="star-8">
-            Rating 8
-          </label>
-
-          <input
-            className="rating__input"
-            id="star-7"
-            type="radio"
-            name="rating"
-            value="7"
-            checked={checked}
-            onChange={() => setChecked(checkboxToggle)}
-          />
-          <label className="rating__label" htmlFor="star-7">
-            Rating 7
-          </label>
-
-          <input
-            className="rating__input"
-            id="star-6"
-            type="radio"
-            name="rating"
-            value="6"
-            checked={checked}
-            onChange={() => setChecked(checkboxToggle)}
-          />
-          <label className="rating__label" htmlFor="star-6">
-            Rating 6
-          </label>
-
-          <input
-            className="rating__input"
-            id="star-5"
-            type="radio"
-            name="rating"
-            value="5"
-            checked={checked}
-            onChange={() => setChecked(checkboxToggle)}
-          />
-          <label className="rating__label" htmlFor="star-5">
-            Rating 5
-          </label>
-
-          <input
-            className="rating__input"
-            id="star-4"
-            type="radio"
-            name="rating"
-            value="4"
-            checked={checked}
-            onChange={() => setChecked(checkboxToggle)}
-          />
-          <label className="rating__label" htmlFor="star-4">
-            Rating 4
-          </label>
-
-          <input
-            className="rating__input"
-            id="star-3"
-            type="radio"
-            name="rating"
-            value="3"
-            checked={checked}
-            onChange={() => setChecked(checkboxToggle)}
-          />
-          <label className="rating__label" htmlFor="star-3">
-            Rating 3
-          </label>
-
-          <input
-            className="rating__input"
-            id="star-2"
-            type="radio"
-            name="rating"
-            value="2"
-            checked={checked}
-            onChange={() => setChecked(checkboxToggle)}
-          />
-          <label className="rating__label" htmlFor="star-2">
-            Rating 2
-          </label>
-
-          <input
-            className="rating__input"
-            id="star-1"
-            type="radio"
-            name="rating"
-            value="1"
-            checked={checked}
-            onChange={() => setChecked(checkboxToggle)}
-          />
-          <label className="rating__label" htmlFor="star-1">
-            Rating 1
-          </label>
         </div>
       </div>
 
