cba0345427069eaf46f4dfbf8c6e3306f7dcb432
kate-mors <ekaterina.morsino@gmail.com>
2021-10-22 21:25:48 +0200

    Remove unused props

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import { films } from './mocks/films';
 
-const onActiveCardHover = (id: number) => id;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App films={films} onActiveCardHover={onActiveCardHover} />
+    <App films={films} />
   </React.StrictMode>,
   document.getElementById('root'));
