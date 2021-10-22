150ab386e11d9d3e6be29bb77da9f3cd989e054d
kate-mors <ekaterina.morsino@gmail.com>
2021-10-22 21:25:31 +0200

    Change Link atribute "to"

--- project/src/components/main-page/main-page.tsx
+++ project/src/components/main-page/main-page.tsx
@@ -68,7 +68,7 @@ function MainPage({ films }: MainScreenProps): JSX.Element {
                 <Link
                   className="btn btn--play film-card__button"
                   type="button"
-                  to={AppRoute.Player}
+                  to={`player/${film.id}`}
                 >
                   <svg viewBox="0 0 19 19" width="19" height="19">
                     <use xlinkHref="#play-s"></use>
