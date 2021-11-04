04de9ecd3528ced821e6b475853358452a2039a2
kate-mors <ekaterina.morsino@gmail.com>
2021-11-04 17:59:37 +0100

    fixup! Create reducer

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -5,29 +5,28 @@ import { DEFAULT_GENRE, SHOWED_FILMS_NUMBER } from '../utils/const';
 
 const initialState = {
   genre: DEFAULT_GENRE,
-  shownFilms: SHOWED_FILMS_NUMBER,
+  shownFilmsNumber: SHOWED_FILMS_NUMBER,
+  initialFilms: films,
   activeFilms: films,
 };
 
 const reducer = (state = initialState, action: Actions): State => {
   switch (action.type) {
     case ActionType.ChangeGenre:
+      if (action.payload === DEFAULT_GENRE) {
+        return {
+          ...initialState,
+        };
+      }
       return {
         ...state,
         genre: action.payload,
-      };
-    case ActionType.ResetGenres:
-      return {
-        ...initialState,
-      };
-    case ActionType.FilterFilmsByGenre:
-      return {
-        ...state,
-        activeFilms: state.activeFilms.filter((film) => film.genre === action.payload),
+        activeFilms: state.initialFilms.filter((film) => film.genre === action.payload),
       };
     default:
       return state;
   }
 };
+
 
 export { reducer };
