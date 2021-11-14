9fc15c72f7f8ff3f46dc4c978c7a5d7df91bdad4
kate-mors <ekaterina.morsino@gmail.com>
2021-11-14 22:29:01 +0100

    Import thunk and Update Store

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './components/app/app';
-import { films } from './mocks/films';
 import { reducer } from './store/reducer';
+import { createAPI } from './components/api/api';
+import { requireAuthorization } from './store/actions';
+import { AuthorizationStatus } from './utils/const';
+import { ThunkAppDispatch } from './types/action';
+import { checkAuthAction, fetchFilmAction } from './store/api-actions';
+
+const api = createAPI(
+  () => store.dispatch(requireAuthorization(AuthorizationStatus.NoAuthorization)),
+);
 
 const store = createStore(
   reducer,
-  composeWithDevTools(),
+  composeWithDevTools(
+    applyMiddleware(thunk.withExtraArgument(api)),
+  ),
 );
+
+(store.dispatch as ThunkAppDispatch)(checkAuthAction());
+(store.dispatch as ThunkAppDispatch)(fetchFilmAction());
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App films={films} />
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
+
