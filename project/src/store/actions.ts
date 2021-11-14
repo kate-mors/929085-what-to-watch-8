4149db70a62c2fb50b94722aeca66f82e891ba60
kate-mors <ekaterina.morsino@gmail.com>
2021-11-14 22:35:19 +0100

    Create new actions in store

--- project/src/store/actions.ts
+++ project/src/store/actions.ts
@@ -1,8 +1,24 @@
-import { ActionType, ChangeGenreAction } from '../types/action';
+import { ActionType } from '../types/action';
+import { FilmsType } from '../types/films';
+import { AuthorizationStatus } from '../utils/const';
 
 
-export const changeGenre = (genre: string): ChangeGenreAction => ({
+export const changeGenre = (genre: string) => ({
   type: ActionType.ChangeGenre,
   payload: genre,
-});
+} as const);
 
+
+export const loadFilms = (data: FilmsType)=> ({
+  type: ActionType.LoadFilms,
+  payload: {data},
+} as const);
+
+export const requireAuthorization = (authorizationStatus: AuthorizationStatus) => ({
+  type: ActionType.RequireAuthorization,
+  payload: authorizationStatus,
+} as const);
+
+export const showMoreFilms = ()=> ({
+  type: ActionType.ShowMoreFilms,
+} as const);
