e608ea67b9ffbc51d9556cd7a378470ffca3aef4
kate-mors <ekaterina.morsino@gmail.com>
2021-11-14 22:32:55 +0100

    Add new action types

--- project/src/types/action.ts
+++ project/src/types/action.ts
@@ -1,10 +1,35 @@
+import { changeGenre, loadFilms, showMoreFilms, requireAuthorization } from '../store/actions';
+import { ThunkAction, ThunkDispatch } from 'redux-thunk';
+import { AxiosInstance } from 'axios';
+import { State } from './state';
+
 export enum ActionType {
   ChangeGenre = 'main/changeGenre',
+  LoadFilms = 'data/loadFilms',
+  ShowMoreFilms = 'main/showMoreFilms',
+  RequireAuthorization = 'user/requireAuthorization',
 }
 
-export type ChangeGenreAction = {
-  type: ActionType.ChangeGenre;
-  payload: string;
-}
+// export type ChangeGenreAction = {
+//   type: ActionType.ChangeGenre;
+//   payload: string;
+// }
 
+// export type LoadFilmsAction = {
+//   type: ActionType.LoadFilms;
+//   payload: [];
+// }
 
+// export type ShowMoreFilmsAction = {
+//   type: ActionType.ShowMoreFilms;
+// }
+
+export type Actions =
+  | ReturnType<typeof changeGenre>
+  | ReturnType<typeof loadFilms>
+  | ReturnType<typeof showMoreFilms>
+  | ReturnType<typeof requireAuthorization>;
+
+export type ThunkActionResult<R = Promise<void>> = ThunkAction<R, State, AxiosInstance, Actions>;
+
+export type ThunkAppDispatch = ThunkDispatch<State, AxiosInstance, Actions>;
