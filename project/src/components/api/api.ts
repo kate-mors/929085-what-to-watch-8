98fb536f9081b169a03ce617b4292224f380a358
kate-mors <ekaterina.morsino@gmail.com>
2021-11-19 14:46:59 +0100

    Connect server with axios

--- project/src/components/api/api.ts
+++ project/src/components/api/api.ts
@@ -1,15 +1,16 @@
-import axios, {AxiosInstance, AxiosResponse, AxiosError} from 'axios';
+import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
+//import {getToken} from '../../utils/token';
 
 const BACKEND_URL = 'https://8.react.pages.academy/wtw';
 const REQUEST_TIMEOUT = 5000;
 
-enum HttpCode {
-   Unauthorized = 401,
-}
+// enum HttpCode {
+//    Unauthorized = 401,
+// }
 
-type UnauthorizedCallback = () => void;
+//type UnauthorizedCallback = () => void;
 
-export const createAPI = (onUnautorized: UnauthorizedCallback): AxiosInstance => {
+export const createAPI = (): AxiosInstance => {
   const api = axios.create({
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
@@ -19,19 +20,11 @@ export const createAPI = (onUnautorized: UnauthorizedCallback): AxiosInstance =>
     (response: AxiosResponse) => response,
 
     (error: AxiosError) => {
-      const { response } = error;
-
-      if (response?.status === HttpCode.Unauthorized) {
-        onUnautorized();
-      }
-
-      return Promise.reject(error);
+      Promise.reject(error);
     },
   );
 
   return api;
 };
 
-// eslint-disable-next-line no-console
-console.log(createAPI);
 
